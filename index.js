1371fad6b4b0d50af54c579da8e401c5e7b350e3
Erickkirui <erickkirui653@gmail.com>
2023-03-15 22:21:32 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide =  (square) =>  {
+    
+return  2000/100
+
+}
+
+console.log(divide())
+
+const square = (multiply) => {
+
+    return multiply * multiply
+} 
+
+console.log(square(10))
+
+const add = (num1 , num2) => {
+
+    return num1 + num2
+}
+console.log(add(12, 12))
